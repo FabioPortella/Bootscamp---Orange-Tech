7c7b4347884373eec0f23f7b7cdf8ef602904cd3
FabioPortella <fabioportella2009@gmail.com>
2022-11-01 21:31:02 -0300

    mais funções

--- Paginas interativas com JavaScript/exerc03.js
+++ Paginas interativas com JavaScript/exerc03.js
@@ -7,24 +7,24 @@
 //3 - Em duas vezes, preço normal de etiqueta sem juros;
 //4 - Acima de duas vezes, preço normal de etiqueta mais juros de 10%;
 
-const precoEtiqueta = 100.00;
-const codPagamento = 4;
-let precoVenda;
+function aplicarDesconto(precoEtiqueta,codPagamento) {
+    let precoVenda;
+    if (codPagamento === 1) {
+        precoVenda = precoEtiqueta * 0.90;
+        console.log(`À vista Débito - 10% de desconto: R\$${precoVenda.toFixed(2)}`);
+    }
+    else if (codPagamento === 2) {
+        precoVenda = precoEtiqueta * 0.85;
+        console.log(`À vista no Dinheiro ou PIX - 15% de desconto: R\$${precoVenda.toFixed(2)}`);
+    }
+    else if (codPagamento === 3) {
+        precoVenda = precoEtiqueta;
+        console.log(`Em duas vezes, preço normal de etiqueta sem juros: R\$${precoVenda.toFixed(2)}`);
+    }
+    else {
+        precoVenda = precoEtiqueta * 1.10;
+        console.log(`Acima de duas vezes, preço normal de etiqueta mais juros de 10%: R\$${precoVenda.toFixed(2)}`);
+    }
+}
 
-
-if (codPagamento === 1) {
-    precoVenda = precoEtiqueta * 0.90;
-    console.log(`À vista Débito - 10% de desconto: R\$${precoVenda.toFixed(2)}`);
-}
-else if (codPagamento === 2) {
-    precoVenda = precoEtiqueta * 0.85;
-    console.log(`À vista no Dinheiro ou PIX - 15% de desconto: R\$${precoVenda.toFixed(2)}`);
-}
-else if (codPagamento === 3) {
-    precoVenda = precoEtiqueta;
-    console.log(`Em duas vezes, preço normal de etiqueta sem juros: R\$${precoVenda.toFixed(2)}`);
-}
-else {
-    precoVenda = precoEtiqueta * 1.10;
-    console.log(`Acima de duas vezes, preço normal de etiqueta mais juros de 10%: R\$${precoVenda.toFixed(2)}`);
-}
+aplicarDesconto(100,1);
