8d06925c2595df99faf05ae529ecc14130624cfb
FabioPortella <fabioportella2009@gmail.com>
2022-11-07 12:54:03 -0300

    exercicio usando importação de um função.

--- Paginas interativas com JavaScript/aula08 - importacao.js
+++ Paginas interativas com JavaScript/aula08 - importacao.js
@@ -1,4 +1,14 @@
 const { gets, print } = require('./funcoes-auxiliares');
 
-print(gets());
+const entradas = [ 5, 50 , 10, 98, 23 ];
+let maiorNumEncontrado = 0;
+
+for (i = 0; i < entradas.length; i++) {
+    if (entradas[i] > maiorNumEncontrado) {
+        maiorNumEncontrado = entradas[i];
+    }
+}
+print(entradas)
+
+print(maiorNumEncontrado);
 
