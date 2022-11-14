bf57e660b487bc7004c9e05dcee91adf083da433
FabioPortella <fabioportella2009@gmail.com>
2022-11-14 01:44:57 -0300

    sem ordenação - somente separar par de impar

--- Desafios/MatrizParImpar.js
+++ Desafios/MatrizParImpar.js
@@ -3,60 +3,16 @@
 // - "print": imprime um texto de saída (output) e pula uma linha ("\n") automaticamente.
 // - ".split(''): transforma a entrada em um array separado por aspas simples
 
-let arr = [1,0]; //let arr = gets().split(''); 
-let arrOrdenado = quickSort(arr, 0, arr.length - 1); 
-let arrPar = [];
-let arrImpar = [];
+let arr = [6,4,3,8,5]; //let arr = gets().split(''); 
 
-for (i=0; i < arrOrdenado.length; i++) {
-    if ((arrOrdenado[i] % 2) === 0) {
-        arrPar.push(arrOrdenado[i]);
-    }
-    else {
-        arrImpar.push(arrOrdenado[i]);
-    }
+let arrVazio = []; 
+
+// TODO: crie a condição necessária para que o array fique ordenado como o exercício pede
+
+arr.forEach(verificarPar);
+
+function verificarPar(item) {
+    if ((item % 2) === 0) {arrVazio.unshift(item)}
+    else {arrVazio.push(item)}
 }
-
-console.log(arrPar.concat(arrImpar));
-
-// função Quick Sort
-
-function swap(items, leftIndex, rightIndex){
-    var temp = items[leftIndex];
-    items[leftIndex] = items[rightIndex];
-    items[rightIndex] = temp;
-}
-
-function partition(items, left, right) {
-    var pivot   = items[Math.floor((right + left) / 2)], //middle element
-        i       = left, //left pointer
-        j       = right; //right pointer
-    while (i <= j) {
-        while (items[i] < pivot) {
-            i++;
-        }
-        while (items[j] > pivot) {
-            j--;
-        }
-        if (i <= j) {
-            swap(items, i, j); //sawpping two elements
-            i++;
-            j--;
-        }
-    }
-    return i;
-}
-
-function quickSort(items, left, right) {
-    var index;
-    if (items.length > 1) {
-        index = partition(items, left, right); //index returned from partition
-        if (left < index - 1) { //more elements on the left side of the pivot
-            quickSort(items, left, index - 1);
-        }
-        if (index < right) { //more elements on the right side of the pivot
-            quickSort(items, index, right);
-        }
-    }
-    return items;
-}
+console.log(arrVazio);
