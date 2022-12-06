28ca6f595c5c102b9e711620d11e3160e664cdbf
FabioPortella <fabioportella2009@gmail.com>
2022-12-06 01:18:13 -0300

    Estilização com Styled-Components

--- dio/src/pages/home/index.jsx
+++ dio/src/pages/home/index.jsx
@@ -1,11 +1,15 @@
 import { Link } from "react-router-dom";
+import { Button } from "../../components/Button";
+import { Header } from "../../components/Header";
+
 
 const Home = () => {
     return (<>
-        <h1>Home</h1>
+        <Header />
+        <Button title="Teste 1" />
+        <Button variant="secondary" title="Teste 2" />
         <Link to="/Login">Fazer login</Link>
-        </>
-    )
+    </>)
 } 
 
 export { Home }
--- dio/src/pages/login/index.jsx
+++ dio/src/pages/login/index.jsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 
+
 const Login = () => {
     return (<>
         <h1>Login</h1>
