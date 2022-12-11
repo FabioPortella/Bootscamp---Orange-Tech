22df0a7e844a2c63081bec84f89e17e6602f8a99
FabioPortella <fabioportella2009@gmail.com>
2022-12-10 22:23:08 -0300

    Tela de Home

--- dio/src/components/Button/styles.js
+++ dio/src/components/Button/styles.js
@@ -4,6 +4,7 @@ export const ButtonContainer = styled.button`
     background: #565656;
     border-radius: 22px;
     position: relative;
+
     color: #FFFF;
     padding: 2px 12px;
     min-width: 120px;
@@ -13,10 +14,16 @@ export const ButtonContainer = styled.button`
     ${({variant})=> variant !== "primary" && css`
         min-width: 167px;
         height: 33px;
+
         background: #E4105D;
 
-        &::after {
-            content: '',
+        &:hover {
+            opacity: 0.6;
+            cursor: pointer;
+        }
+
+        &:after {
+            content: '';
             position: absolute;
             border: 1px solid #e4105d;
             top: -5px;
--- dio/src/pages/home/index.jsx
+++ dio/src/pages/home/index.jsx
@@ -1,11 +1,33 @@
 import { Link } from "react-router-dom";
+import bannerImage from '../../assets/bannerImage.png';
 import { Button } from "../../components/Button";
 import { Header } from "../../components/Header";
+
+import { Container, TextContent, Title, TitleHighLight } from './styles'
 
 
 const Home = () => {
     return (<>
         <Header />
+        <Container>
+            <div>
+                <Title>
+                    <TitleHighLight>
+                        Implemente
+                        <br />
+                    </TitleHighLight>
+                    o seu futuro global agora!
+                </Title>
+                <TextContent>
+                    Domine as tecnologias utilizadas pelas empresas mais novadoras do mundo e encare seu novo 
+                    desafio profissional, evoluindo em comunidade com os melhores experts.
+                </TextContent>
+                <Button title="Começar agora" variant="secondary" onClick={() => null} />
+            </div>
+            <div>
+                <img src={ bannerImage } alt="Imagem principal" />
+            </div>
+        </Container>
     </>)
 } 
 
