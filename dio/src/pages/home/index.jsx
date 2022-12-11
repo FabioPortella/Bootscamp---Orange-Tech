3a72fbbe7e04efc6a12103f940a113853b2bd5d5
FabioPortella <fabioportella2009@gmail.com>
2022-12-10 21:25:55 -0300

    Conhecendo a aplicação e construindo componentes

--- dio/src/pages/home/index.jsx
+++ dio/src/pages/home/index.jsx
@@ -6,9 +6,6 @@ import { Header } from "../../components/Header";
 const Home = () => {
     return (<>
         <Header />
-        <Button title="Teste 1" />
-        <Button variant="secondary" title="Teste 2" />
-        <Link to="/Login">Fazer login</Link>
     </>)
 } 
 
