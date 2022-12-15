713c72f02d02ff3e2257dd5daa870d0607a915d2
FabioPortella <fabioportella2009@gmail.com>
2022-12-14 23:49:13 -0300

    Finalizano o coponente Fedd

--- dio/src/pages/feed/index.jsx
+++ dio/src/pages/feed/index.jsx
@@ -1,21 +1,31 @@
-import { Link } from "react-router-dom";
-import bannerImage from '../../assets/bannerImage.png';
-import { Button } from "../../components/Button";
 import { Card } from "../../components/Card";
 import { UserInfo } from "../../components/UserInfo";
 import { Header } from "../../components/Header";
 
-import { Container, TextContent, Title, TitleHighLight } from './styles';
+import { Container, Column, Title, TitleHighLight } from './styles';
 
 
 const Feed = () => {
     return (<>
         <Header />
         <Container>
-            <Card />
-            <UserInfo percentual={88}                   
-                nome="Fábio Portella" 
-                image="https://avatars.githubusercontent.com/u/45184516?v=4" />                
+            <Column flex={3}>
+                <Title>Fedd</Title>
+                <Card /> 
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+            </Column>
+            <Column flex={1}>
+                <TitleHighLight># RANKING 5 TOP DA SEMANA</TitleHighLight>
+                <UserInfo percentual={80} nome="Fábio Portella" image="https://avatars.githubusercontent.com/u/45184516?v=4" />
+                <UserInfo percentual={27} nome="Pablo Henrique" image="https://avatars.githubusercontent.com/u/45184516?v=4" />
+                <UserInfo percentual={35} nome="Haniel Sonego" image="https://avatars.githubusercontent.com/u/45184516?v=4" />
+                <UserInfo percentual={57} nome="Carlos Costa" image="https://avatars.githubusercontent.com/u/45184516?v=4" /> 
+                <UserInfo percentual={12} nome="João Pedro Ussem" image="https://avatars.githubusercontent.com/u/45184516?v=4" />
+            </Column>                                       
         </Container>
     </>)
 } 
