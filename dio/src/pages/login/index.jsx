3aea1b0153a4c85452f8495e007286dd08a53320
FabioPortella <fabioportella2009@gmail.com>
2022-12-16 22:15:06 -0300

    pequenas correções

--- dio/src/pages/login/index.jsx
+++ dio/src/pages/login/index.jsx
@@ -6,19 +6,21 @@ import * as yup from "yup";
 
 import { Button } from "../../components/Button";
 import { Header } from "../../components/Header";
-import { Input } from "../../components/Input";
-
-import { api } from '../../services/api';
+import { Input }  from "../../components/Input";
+import { api }    from '../../services/api';
 import { Column, Container, CriarText, 
          EsqueciText, Row, SubtitleLogin, 
          Title, TitleLogin, Wrapper} from './styles'
+
 
 const schema = yup.object({
     email: yup.string().email('E-mail não é válido.').required('Campo obrigatório.'),
     password: yup.string().min(3, 'No mínimo 3 caracteres.').required('Campo obrigatório.'),
   }).required();
 
+
 const Login = () => {
+
     const navigate = useNavigate();
 
     const { control, handleSubmit, formState: { errors } } = useForm({
@@ -29,6 +31,7 @@ const Login = () => {
     const onSubmit = async formData => {
         try{
             const { data } = await api.get(`users?email=${formData.email}&senha=${formData.password}`);
+            
             if(data.length === 1){
                 navigate('/feed');
             } else {
@@ -54,9 +57,21 @@ const Login = () => {
                     <TitleLogin>Faça seu cadastro</TitleLogin>
                     <SubtitleLogin>Faça seu login e make the change._</SubtitleLogin>
                     <form onSubmit={handleSubmit(onSubmit)}>
-                        <Input name="email" errorMessage={errors?.email?.message} control={control} placeholder="E-mail"  />
-                        <Input name="password" errorMessage={errors?.password?.message} control={control} placeholder="Senha" type="password"  />
-                        <Button title="Entrar" variant="secondary" type="submit" />
+                        <Input 
+                            name="email" 
+                            errorMessage={errors?.email?.message} 
+                            control={control} 
+                            placeholder="E-mail"  />
+                        <Input 
+                            name="password" 
+                            errorMessage={errors?.password?.message} 
+                            control={control} 
+                            placeholder="Senha" 
+                            type="password"  />
+                        <Button 
+                            title="Entrar" 
+                            variant="secondary" 
+                            type="submit" />
                     </form>
                     <Row>
                         <EsqueciText>Esqueci minha senha</EsqueciText>
