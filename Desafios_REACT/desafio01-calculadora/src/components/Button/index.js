ea5163058ae43aabd85e7d50841bcdd13baf728b
FabioPortella <fabioportella2009@gmail.com>
2022-12-19 13:10:50 -0300

    estilizando Input

--- Desafios_REACT/desafio01-calculadora/src/components/Button/index.js
+++ Desafios_REACT/desafio01-calculadora/src/components/Button/index.js
@@ -1,7 +1,7 @@
 const Button = () => {
     return (
       <div className="App">
-         Olá Mundo
+         Olá
       </div>
     );
   }
--- Desafios_REACT/desafio01-calculadora/src/components/Input/styles.js
+++ Desafios_REACT/desafio01-calculadora/src/components/Input/styles.js
@@ -1,1 +1,11 @@
 import styled from "styled-components";
+
+export const InputContainer = styled.div`
+    width: 100%;
+    height: 75px;
+    background-color: #aaffaa;
+
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+`
