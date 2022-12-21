8505e6e74734133d8b40aedc2025d2c3c5e28638
FabioPortella <fabioportella2009@gmail.com>
2022-12-21 12:42:33 -0300

    Adicionano component -> ItemRepo com styles

---
+++ Desafios_REACT/desafio02-github-wiki/src/components/ItemRepo/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import { ItemContainer } from './styles';
+
+ function ItemRepo() {
+  return (
+    <ItemContainer>
+        <h3>Fábio Portella</h3>
+        <p> dio/fábio-ports</p>
+        <a href='#'>Remover</a>
+        <hr />
+    </ItemContainer>
+  )
+}
+
+export default ItemRepo
